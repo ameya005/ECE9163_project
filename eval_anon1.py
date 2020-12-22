c21c442f70939ff3a31b73f0b20bfa0055ded682
ameya005 <ameya@iastate.edu>
2020-12-22 15:20:28 -0500

    Added image level evaluation scripts for all 4 models

---
+++ eval_anon1.py
@@ -0,0 +1,29 @@
+"""
+Eval script
+"""
+
+import keras
+import sys
+from PIL import Image
+from eval import data_preprocess
+import numpy as np
+
+model_path = './models/anonymous_bd_net.h5'
+rep_model_path = './models/anonymous_bd_net_repaired.h5'
+
+img = np.array(Image.open(sys.argv[1]))
+img = np.expand_dims(img, 0) # bs, sx, sy, ch
+img = data_preprocess(img)
+
+orig_model = keras.models.load_model(model_path)
+rep_model = keras.models.load_model(rep_model_path)
+
+orig_pred = np.argmax(orig_model.predict(img), axis=1)
+rep_pred = np.argmax(rep_model.predict(img), axis=1)
+
+if orig_pred == rep_pred:
+    print(orig_pred)
+else:
+    print(-1)
+
+
